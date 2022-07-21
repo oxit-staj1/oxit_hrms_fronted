ce9276f1b85f1da258087fae110312b1ed6d23af
Erebuss0 <109722528+Erebuss0@users.noreply.github.com>
2022-07-21 11:54:35 +0300

    Update Flags.js

--- src/views/icons/flags/Flags.js
+++ src/views/icons/flags/Flags.js
@@ -54,7 +54,7 @@ const Cards = () => {
                         />
                       </label>
                       <label>
-                        Doğum Günü
+                        Doğum Günüüüüüüüüüüüüüüü
                         <input
                           class="form-control"
                           type="date"
