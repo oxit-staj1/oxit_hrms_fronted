1ef2425592f15f7f37280d961c99ca6980605277
Tugba-Ariyan <tugbaariyan@gmail.com>
2022-07-21 14:11:42 +0300

    Update Cards.js

--- src/views/base/cards/Cards.js
+++ src/views/base/cards/Cards.js
@@ -1,5 +1,5 @@
-import React ,{useState,useEffect} from 'react'
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import {
   CButton,
@@ -20,33 +20,32 @@ import {
   CNavLink,
   CCol,
   CRow,
-} from '@coreui/react'
-import { DocsExample } from 'src/components'
+} from "@coreui/react";
+import { DocsExample } from "src/components";
 
-import ReactImg from 'src/assets/images/react.gif'
-import HizmetService from 'src/service/hizmetService'
+import ReactImg from "src/assets/images/react.gif";
+import HizmetService from "src/service/hizmetService";
 
 const Cards = () => {
-  
-  const [hizmets, setHizmets] = useState({})
+  const [hizmets, setHizmets] = useState({});
 
   useEffect(() => {
     const hizmetService = new HizmetService();
-    hizmetService.getHizmet().then((result) => setHizmets(result.data.data))
-  },[])
+    hizmetService.getHizmet().then((result) => setHizmets(result.data.data));
+  }, []);
 
-
-  const handleChange = e => {
+  const handleChange = (e) => {
     const value = e.target.value;
-    setHizmets({hizmet_ad:value});
+    setHizmets({ hizmet_ad: value });
   };
 
-
-  const handleSubmit  = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post("http://localhost:8080/api/hizmet/add", hizmets).then((response) => {
-      console.log(response.status);
-    });
+    axios
+      .post("http://localhost:8080/api/hizmet/add", hizmets)
+      .then((response) => {
+        console.log(response.status);
+      });
   };
   return (
     <CRow>
@@ -56,36 +55,37 @@ const Cards = () => {
             <strong>Hizmet</strong> <small>Ekleme</small>
           </CCardHeader>
           <CCardBody>
-
             <DocsExample href="components/card">
-              <CCard style={{ width: '18rem' }}>
+              <CCard style={{ width: "18rem" }}>
                 <CCardImage orientation="top" src={ReactImg} />
                 <CCardBody>
-                  <CCardTitle>Eklemek İstediğiniz Hizmet Türünü Giriniz</CCardTitle>
-                  <CCardText>
-
-                  </CCardText>
+                  <CCardTitle>
+                    Eklemek İstediğiniz Hizmet Türünü Giriniz
+                  </CCardTitle>
+                  <CCardText></CCardText>
                   <form onSubmit={handleSubmit}>
                     <label htmlFor="hizmet ad ">
                       Hizmet Ad :
                       <input
+                        class="form-control"
                         type="text"
                         name="hizmetAd"
                         value={hizmets.hizmet_ad}
                         onChange={handleChange}
                       />
                     </label>
-                    <button type="submit">add</button>
+                    <button class="btn btn-light" type="submit">
+                      Ekle
+                    </button>
                   </form>
                 </CCardBody>
-               
               </CCard>
             </DocsExample>
           </CCardBody>
         </CCard>
       </CCol>
     </CRow>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
