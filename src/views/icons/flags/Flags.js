a554d6ee4cdfde2c36ad2ca9457c955277c8fc4e
Erebuss0 <109722528+Erebuss0@users.noreply.github.com>
2022-07-21 11:15:35 +0300

    Update Flags.js

--- src/views/icons/flags/Flags.js
+++ src/views/icons/flags/Flags.js
@@ -62,7 +62,7 @@ const Cards = () => {
                         />
                       </label>
                       <label>
-                        Birimiiiilllllll
+                        Birimiiiilllllllaaaaaaa
                         <input class="form-control" type="text" name="birim" />
                       </label>
                       <br></br>
