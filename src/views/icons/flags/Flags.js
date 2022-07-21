755297262d3163de8c521e69d2985aa600c27f51
kayaomerr <102926530+kayaomerr@users.noreply.github.com>
2022-07-21 11:34:55 +0300

    Update Flags.js

--- src/views/icons/flags/Flags.js
+++ src/views/icons/flags/Flags.js
@@ -62,7 +62,7 @@ const Cards = () => {
                         />
                       </label>
                       <label>
-                        Birimiiii
+                        Birimi
                         <input class="form-control" type="text" name="birim" />
                       </label>
                       <br></br>
