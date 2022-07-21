afd88b8295ce23e73088cd514cb4ba6ad02648b3
kayaomerr <102926530+kayaomerr@users.noreply.github.com>
2022-07-21 13:50:23 +0300

    Create StajApplication.java

---
+++ src/main/java/com/cagri/staj/StajApplication.java
@@ -0,0 +1,13 @@
+package com.cagri.staj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StajApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StajApplication.class, args);
+    }
+
+}
