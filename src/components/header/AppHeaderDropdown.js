9b92796a370c1da01fc5aacc2796f3517ca8ed82
Erebuss0 <109722528+Erebuss0@users.noreply.github.com>
2022-07-25 12:30:16 +0300

    s

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -23,7 +23,7 @@ import {
 } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 
-import avatar8 from './../../assets/images/avatars/8.jpg'
+import avatar8 from './../../assets/images/avatars/logo-kafa.jfif'
 
 const AppHeaderDropdown = () => {
   return (
