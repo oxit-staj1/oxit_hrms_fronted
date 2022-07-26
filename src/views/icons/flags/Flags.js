9bdc775c658cb04a979aa6c6f5f5d5c4e7ed03bd
Çağrı KART <ckarte43@gmail.com>
2022-07-26 14:11:35 +0300

    Update Flags.js

--- src/views/icons/flags/Flags.js
+++ src/views/icons/flags/Flags.js
@@ -22,16 +22,36 @@ import {
 import { DocsExample } from "src/components";
 
 import ReactImg from "src/assets/images/react.gif";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Cards = () => {
+  const [personel_id, setPersonel_id] = useState([]);
+  const [personel_ad, setPersonel_ad] = useState([]);
+  const [personel_soyad, setPersonel_soyad] = useState([]);
+  const [is_baslangic_tarihi, setİs_baslangic_tarihi] = useState([]);
+  const [dogum_tarihi, setDogum_tarihi] = useState([]);
+
+  
+const url = "http://localhost:8080/api/personel/add"
+const postData = () => {
+  axios.post(url, {
+    personel_ad,
+    personel_soyad,
+    is_baslangic_tarihi,
+    dogum_tarihi
+  }).then(() => {
+    history.push('icons/coreui-icons') })
+}
   return (
     <CRow>
       <CCol xs={12}>
         <CCard className="mb-4">
           <CCardBody>
+            PERSONELLER
             <DocsExample href="components/card">
               <CCard style={{ width: "18rem" }}>
-                <a href="/#/icons/coreui-icons"><CCardImage orientation="top" src={ReactImg}/></a>
+                <CCardImage orientation="top" src={ReactImg} />
                 <CCardBody>
                   <CCardTitle>Personel Ekle</CCardTitle>
                   <CCardText></CCardText>
@@ -39,54 +59,52 @@ const Cards = () => {
                     <div class="form-group">
                       <label>
                         Ad
-                        <input class="form-control" type="text" name="ad" />
+                        <input class="form-control" type="text" name="ad" onChange={(e) => setPersonel_ad(e.target.value)} />
                       </label>
                       <label>
                         Soyad
-                        <input class="form-control" type="text" name="soyad" />
+                        <input class="form-control" type="text" name="soyad" onChange={(e) => setPersonel_soyad (e.target.value)} />
                       </label>
                       <label>
                         Başlama Tarihi
                         <input
                           class="form-control"
-                          type="date"
+                          type="text"
                           name="baslama"
+                          onChange={(e) => setİs_baslangic_tarihi(e.target.value)}
                         />
                       </label>
                       <label>
                         Doğum Günü
                         <input
                           class="form-control"
-                          type="date"
+                          type="text"
                           name="birthday"
+                          onChange={(e) => setDogum_tarihi(e.target.value)}
                         />
                       </label>
                       <label>
                         Birimi
+
                         <input class="form-control" type="text" name="birim" />
                       </label>
                       <br></br>
 
                       <br></br>
-                      <form action="" method="post" enctype="multipart/form-data">
-                  <input type="file" name="dosya" />
-                  <br>
-                  </br>
-
-                  
-                  <br></br>
-
-              </form>
-
-                
-                      
-                      <input type="submit" value="Ekle" />
+                  {/*     <form action="" method="post" enctype="multipart/form-data">
+                        <input type="file" name="dosya" />
+                        <br>
+                        </br>
+                        <br></br>
+                      </form> */}
+                      <input onClick={postData} type="submit" value="Ekle" />
                     </div>
                   </form>
                 </CCardBody>
                 <CCardBody></CCardBody>
               </CCard>
             </DocsExample>
+            
           </CCardBody>
         </CCard>
       </CCol>
