02896f0c683881846f8c884979f2d63120ba5474
Erebuss0 <109722528+Erebuss0@users.noreply.github.com>
2022-08-04 11:19:05 +0300

    b

--- src/views/base/cards/Cards.js
+++ src/views/base/cards/Cards.js
@@ -1,1 +1,126 @@
-a
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardGroup,
+  CCardHeader,
+  CCardImage,
+  CCardLink,
+  CCardSubtitle,
+  CCardText,
+  CCardTitle,
+  CListGroup,
+  CListGroupItem,
+  CNav,
+  CNavItem,
+  CNavLink,
+  CCol,
+  CRow,
+} from "@coreui/react";
+import { DocsExample } from "src/components";
+
+import ReactImg from "src/assets/images/react.gif";
+import HizmetService from "src/service/hizmetService";
+import { useNavigate } from "react-router-dom";
+import HizmetList from "./HizmetList";
+import FormControl from "src/views/forms/form-control/FormControl";
+
+const Cards = () => {
+  let history = useNavigate();
+  const urlAdd= "http://localhost:8080/api/hizmet/add";
+
+  const [hizmet_ad, setHizmet_ad] = useState([]);
+
+
+  const getData = () => {
+    axios.get(urlGet)
+        .then((getData) => {
+            setHizmets(getData.data.data);
+        })
+}
+const urlGet = "http://localhost:8080/api/hizmet/getall";
+const [hizmets, setHizmets] = useState([]);
+useEffect(() => {
+    axios.get(urlGet)
+        .then((response) => {
+            setHizmets(response.data.data);
+            console.log(response.data.data)
+        })
+}, [])
+  const postData = () => {
+
+    const response = axios.post(urlAdd, JSON.stringify({ hizmet_ad }),
+      {
+        headers: { 'Content-Type': 'application/json' }
+      })
+      .then(() => {
+        window.location.reload();
+      })
+
+
+  }
+
+
+  return (
+    <CRow>
+      <CCol xs={12}>
+        <CCard className="mb-4">
+          <CCardHeader>
+            <strong>Hizmet Ekleme</strong>
+          </CCardHeader>
+          <CCard>
+         
+          
+          
+          </CCard>
+        
+          <CCardBody>
+            <DocsExample href="components/card">
+              <CCard style={{ width: "18rem" }}>
+                <CCardBody>
+                  <CCardTitle>
+                    Eklemek İstediğiniz Hizmet Türünü Giriniz
+                  </CCardTitle>
+                  <CCardText></CCardText>
+                  
+                  <form className="create-form">
+                  
+                    <input
+                      class="form-control"
+                      placeholder="Hizmet adını giriniz"
+                      onChange={(e) => setHizmet_ad(e.target.value)}
+                    />
+                    <br />
+                    <button
+                      class="btn btn-secondary btn-sm"
+                      onClick={postData}
+                      type="submit"
+                      value="submit"
+                    >
+                      {" "}
+                      Ekle{" "}
+                    </button>
+                    
+                  </form>
+                  
+                </CCardBody>
+
+              </CCard>
+
+            </DocsExample>
+            <HizmetList />
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+    
+
+  );
+ 
+};
+
+export default Cards;
--- src/views/base/cards/HizmetList.js
+++ src/views/base/cards/HizmetList.js
@@ -1,1 +1,71 @@
-a
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Button, Table } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+
+const HizmetList = () => {
+    const url = "http://localhost:8080/api/hizmet/getall";
+    const [hizmets, setHizmets] = useState([]);
+    useEffect(() => {
+        axios.get(url)
+            .then((response) => {
+                setHizmets(response.data.data);
+                console.log(response.data.data)
+            })
+    }, [])
+
+    const setData = (hizmet) => {
+        let { hizmet_id, hizmet_ad } = hizmet;
+        localStorage.setItem('hizmet_id', hizmet_id);
+        localStorage.setItem('hizmet_ad', hizmet_ad);
+    }
+
+    const getData = () => {
+        axios.get(url)
+            .then((getData) => {
+                setHizmets(getData.data.data);
+            })
+    }
+
+    const onDelete = (hizmet_id) => {
+        axios.delete(`http://localhost:8080/api/hizmet/delete/${hizmet_id}`)
+            .then(() => {
+                getData();
+            })
+    }
+    return (
+        <div>
+            <Table class="table table striped">
+
+                <Table.Header>
+                    <p>
+                    <Table.Row>
+                        <Table.HeaderCell>Hizmet Listesi</Table.HeaderCell>
+                    </Table.Row>
+                    </p>
+                </Table.Header>
+
+                <Table.Body>
+                    {hizmets.map((hizmet) => {
+                        return (
+                            <Table.Row>
+                                
+                                <Table.Cell><th>-->      {hizmet.hizmet_ad .toUpperCase()}</th></Table.Cell>
+                                    <Table.Cell>
+                                        <p>
+                                        <Button type="button" class=" btn-secondary" onClick={() => setData(hizmet)}>Update</Button>
+                                        <Button type="button" class ="btn-secondary" onClick={() => onDelete(hizmet.hizmet_id)}>Delete</Button>
+                                        </p>
+                                    </Table.Cell>
+                    
+                            </Table.Row>
+                        )
+                    })}
+                </Table.Body>a
+            </Table>
+        </div>
+    )
+}
+
+export default HizmetList
